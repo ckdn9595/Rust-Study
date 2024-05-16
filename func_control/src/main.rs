c7eb885d5048d254982aae96bcad4e3c39e69b5a
JiwooLee <ckdn9595@gmail.com>
2024-05-16 23:48:19 +0900

    3단원 학습 완료

--- func_control/src/main.rs
+++ func_control/src/main.rs
@@ -7,6 +7,10 @@ fn main() {
     test_void(num, day_unit);
     println!("{}", test_return(num, day_unit));
     test_if(num);
+    let count = test_loop();
+    println!("the loop test result is {}", count);
+    test_multiple_loop();
+    test_for();
 }
 fn test_void(num: u32, day_unit: &str) {
     println!("1 오늘은 {num}{day_unit}입니다.");
@@ -16,10 +20,10 @@ fn test_return(num: u32, day_unit: &str) -> String {
     str
 }
 
-fn test_if(num: u32){
+fn test_if(num: u32) {
     if num > 1 {
         println!("true");
-    }else{
+    } else {
         println!("false");
     }
 
@@ -31,6 +35,67 @@ fn test_if(num: u32){
         println!("num is not divisible");
     }
 
-    let bigger_num = if num > 6 {num} else {6};
+    let bigger_num = if num > 6 { num } else { 6 };
     println!("The bigger_num is: {bigger_num}");
+}
+
+fn test_loop() -> u32 {
+    let mut count = 0;
+    loop {
+        //count++; <- 이거 안됨
+        count += 1;
+        if count == 10 {
+            break count;
+        }
+    }
+}
+
+fn test_multiple_loop() {
+    // 1 ~ 4 를 가진 카드 4장에서 2장을 뽑는 이중 포문
+    let cards: [u8; 4] = [1, 2, 3, 4];
+    let mut cnt = 0;
+    let mut a_index = 0;
+    let mut b_index = a_index + 1;
+    'first: loop {
+        loop {
+            if a_index == 4 {
+                break 'first;
+            }
+            if b_index == 4 {
+                break;
+            }
+            cnt += 1;
+            println!("{}번째 : {}, {}", cnt, cards[a_index], cards[b_index]);
+            b_index += 1;
+        }
+        a_index += 1;
+        b_index = a_index + 1;
+    }
+}
+fn test_while() {
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+}
+
+fn test_for() {
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {element}");
+    }
+
+    for number in (1..4) {
+        println!("{number}!");
+    }
+
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
 }
